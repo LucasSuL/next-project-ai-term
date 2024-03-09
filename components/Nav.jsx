88e54a777cf909ec6783f147432c24f758ee797a
Lucas Su <lijie.su93@gmail.com>
2024-03-09 23:37:54 +1030

    finish: google auth env

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,6 +8,7 @@ import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 const Nav = () => {
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggle, setToggle] = useState(false);
   useEffect(() => {
     const setProviders = async () => {
       const response = await getProviders();
@@ -52,7 +53,72 @@ const Nav = () => {
             {providers &&
               Object.values(providers).map((provider) => {
                 <button
-                type="button" key={provider.name} onClick={()=>{signIn(provider.id)} }className='black_btn'>Sign In</button>;
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
+              })}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggle((prev) => !prev)}
+            ></Image>
+            {toggle && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggle(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggle(false)}
+                >
+                  Create Translation
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setToggle(false);
+                    signOut();
+                  }} className="mt-5 w-full black_btn"
+                >Sign Out</button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>;
               })}
           </>
         )}
